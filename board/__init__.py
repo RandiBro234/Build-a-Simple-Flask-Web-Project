91d1df0a47dd75dc3f30d690247d4f5d772dce55
Randi Nandika Danendra <randizelda30@gmail.com>
2025-09-20 20:07:35 +0000

    Resolve merge conflicts by removing .pyc files and venv config

--- board/__init__.py
+++ board/__init__.py
@@ -3,5 +3,9 @@ from board import pages
 
 def create_app():
     app = Flask(__name__)
+<<<<<<< HEAD
+=======
+
+>>>>>>> a6a3d88 (Initial commit project Flask message board)
     app.register_blueprint(pages.bp)
     return app
